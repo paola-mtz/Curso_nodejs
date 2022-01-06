8018dc23ed7564b9922dc638e6e0808b6dcfb0c1
paola-mtz <Isc18350324@gmail.com>
2022-01-06 15:43:34 -0600

    Se hizo el .env y se corrigio el .gitignore

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express=require('express');
-
+require("dotenv").config();
 const app =express();
 
 app.get('/', function(req,res){res.send('Hola Mundo');});
 
-app.listen(3000);
+app.listen(process.env.PORT);
 
+//localhost:3000
 //localhost:3000
