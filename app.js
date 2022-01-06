5e082c9396e805e155a5f1d269097aed31f15b07
paola-mtz <Isc18350324@gmail.com>
2022-01-06 14:59:59 -0600

    Se agrego express y dotenv

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo");
+const express=require('express');
+
+const app =express();
+
+app.get('/', function(req,res){res.send('Hola Mundo');});
+
+app.listen(3000);
+
+//localhost:3000
