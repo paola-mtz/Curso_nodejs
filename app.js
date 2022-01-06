d5430876e6d6122497e21850f0698f9586531a39
paola-mtz <Isc18350324@gmail.com>
2022-01-06 16:38:36 -0600

    Estructura inicial de la ApI

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const express=require('express');
 require("dotenv").config();
-const app =express();
-
-app.get('/', function(req,res){res.send('Hola Mundo');});
-
-app.listen(process.env.PORT);
-
-//localhost:3000
-//localhost:3000
+const Server= require('./server');
+const server=new Server;
+server.listen();
---
+++ server.js
@@ -0,0 +1,22 @@
+const express =require('express');
+
+class Server{
+    constructor(){
+        this.app=express();
+        this.port=process.env.PORT;
+
+        this.routers;
+    }
+
+    routers(){
+        this.app.get('/', function(req,res){res.send('Hola Mundo');});
+
+    }
+
+    listen(){
+        this.app.listen(this.port);
+    }
+
+}
+
+module.exports=Server;
