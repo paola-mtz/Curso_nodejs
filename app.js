04b967e49568fc02c2d4857aea6c9755b3135b31
paola-mtz <Isc18350324@gmail.com>
2022-01-07 15:38:33 -0600

    Actualización de Archivos (server, app y .env)

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-require("dotenv").config();
-const Server= require('./server');
-const server=new Server;
+
+const dotenv =require('dotenv').config();
+
+const Server=require('./server')
+
+const server= new Server;
+
 server.listen();
+
+
+
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express =require('express');
+const cors = require("cors");
 
 class Server{
     constructor(){
         this.app=express();
-        this.port=process.env.PORT;
-
-        this.routers;
+        this.PORT=process.env.PORT;
+        this.rutasUsuarios="/usuarios";
+        
+        this.middlewares();
+        this.routes();
     }
 
-    routers(){
-        this.app.get('/', function(req,res){res.send('Hola Mundo');});
+    middlewares(){
+        this.app.use(cors());
+        this.app.use(express.json());
+    };
 
+    routes(){
+        this.app.use(this.rutasUsuarios, require ("./routes/usuarios"));
     }
 
-    listen(){
-        this.app.listen(this.port);
+    listen (){
+        this.app.listen(process.env.PORT); 
     }
-
-}
+} 
 
 module.exports=Server;
